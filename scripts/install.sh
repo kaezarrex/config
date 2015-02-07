8bb20a8497d311fe3c68f664c4d9dae4a0056471
David Hazinski <dhazinski@gmail.com>
2015-02-07 16:25:01 +0000

    fixing scripts path

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # The path containing the scripts.
-export SCRIPTS_PATH=$(pwd)
+export SCRIPTS_PATH=$(dirname $(readlink -f $0))
 
 . $SCRIPTS_PATH/config.sh
 . $SCRIPTS_PATH/functions.sh
